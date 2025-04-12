641787a27fe5b72cd8855ab26981f44b0cebbef9
Shikhar Katiyar <148053313+myselfshikhar@users.noreply.github.com>
2025-04-12 23:29:40 +0530

    added home route

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,10 +19,10 @@ app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
-// Home route
-// app.get("/", (req, res) => {
-//   res.send("API is running...");
-// });
+Home route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 app.use("/api/v1/users", userRoutes);
 
