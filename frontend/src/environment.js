db2ab13a8feb1105c4456bbf3cf5137ffc4dd4aa
Shikhar Katiyar <148053313+myselfshikhar@users.noreply.github.com>
2025-04-12 23:03:31 +0530

    added production link

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
-let IS_PROD = false;
+let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+    "https://video-call-meeting-7aov.onrender.com" :
 
     "http://localhost:8000"
 
